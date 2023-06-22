dcd5a7b141d23185061fc56bda61e65590c7c7e0
Alwaleed <alwaleed.hamam@gmail.com>
2023-06-21 22:23:31 -0400

    Create data classes for the API response

---
+++ app/src/main/java/com/scotia/androidtakehomeassigment/domain/model/GithubUser.kt
@@ -0,0 +1,10 @@
+package com.scotia.androidtakehomeassigment.domain.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class GithubUser(
+    @Json(name = "name") val name: String,
+    @Json(name = "avatar_url") val avatarUrl: String
+)
