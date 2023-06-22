da710e953874ad3a2d703f1f0ff932b065f5e752
Alwaleed <alwaleed.hamam@gmail.com>
2023-06-21 22:13:27 -0400

    Add dependencies and configure Retrofit and Moshi

---
+++ app/src/main/java/com/scotia/androidtakehomeassigment/data/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.scotia.androidtakehomeassigment.data.di
+
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(moshi: Moshi): Retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideGithubApi(retrofit: Retrofit): GithubApi =
+        retrofit.create(GithubApi::class.java)
+}
